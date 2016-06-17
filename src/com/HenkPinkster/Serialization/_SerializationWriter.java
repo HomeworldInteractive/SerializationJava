ea776cc54ab11411b30b7c998ea9c382dda15d6a
henk pinkster <henk pinkster>
2016-06-17 11:04:07 -0700

    added float and boolean to serialization writer

--- src/com/HenkPinkster/Serialization/_SerializationWriter.java
+++ src/com/HenkPinkster/Serialization/_SerializationWriter.java
@@ -16,6 +16,12 @@ public class _SerializationWriter {
 		return pointer;
 	}
 	
+	public static int writeBytes(byte[] dest, int pointer, char value){
+		dest[pointer++] = (byte)((value >> 8) & 0xff);
+		dest[pointer++] = (byte)((value >> 0) & 0xff);
+		return pointer;
+	}
+	
 	public static int writeBytes(byte[] dest, int pointer, int value){
 		dest[pointer++] = (byte)((value >> 24) & 0xff);
 		dest[pointer++] = (byte)((value >> 16) & 0xff);
@@ -33,6 +39,21 @@ public class _SerializationWriter {
 		dest[pointer++] = (byte)((value >> 16) & 0xff);
 		dest[pointer++] = (byte)((value >> 8) & 0xff);
 		dest[pointer++] = (byte)((value >> 0) & 0xff);
+		return pointer;
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, float value){
+		int data = Float.floatToIntBits(value);
+		return writeBytes(dest, pointer, data);
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, double value){
+		long data = Double.doubleToLongBits(value);
+		return writeBytes(dest, pointer, data);
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, boolean value){
+		dest[pointer++] = (byte)(value ? 1 : 0);
 		return pointer;
 	}
 }
